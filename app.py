3619f6f46b4c87c22a21662a690d1e8414e02e82
Ansel <ansel.yu@gmail.com>
2024-10-02 17:36:10 +0300

    Test main app

--- app.py
+++ app.py
@@ -1,25 +1,25 @@
-# Run this app with `python app.py` and
-# visit http://127.0.0.1:8050/ in your web browser.
-
-from dash import Dash, dcc, html
-import plotly.express as px
-import pandas as pd
+from dash import Dash, dcc, html, Input, Output, callback
+import os
 
 
-app = Dash(__name__)
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
-df = pd.read_csv('https://gist.githubusercontent.com/chriddyp/5d1ea79569ed194d432e56108a04d188/raw/a9f9e8076b837d541398e999dcbac2b2826a81f8/gdp-life-exp-2007.csv')
+app = Dash(__name__, external_stylesheets=external_stylesheets)
 
-fig = px.scatter(df, x="gdp per capita", y="life expectancy",
-                 size="population", color="continent", hover_name="country",
-                 log_x=True, size_max=60)
+server = app.server
 
 app.layout = html.Div([
-    dcc.Graph(
-        id='life-exp-vs-gdp',
-        figure=fig
-    )
+    html.H1('Hello World'),
+    dcc.Dropdown(['LA', 'NYC', 'MTL'],
+        'LA',
+        id='dropdown'
+    ),
+    html.Div(id='display-value')
 ])
 
+@callback(Output('display-value', 'children'), Input('dropdown', 'value'))
+def display_value(value):
+    return f'You have selected {value}'
+
 if __name__ == '__main__':
-    app.run_server(debug=True)
+    app.run(debug=True)
